cf74bda5d93766fd84d2da847a9a6dc18e4f5d9a
Moskalenko Serhii <mynameserhii@gmail.com>
2022-08-06 19:58:10 +0300

    Routes, pages, components

---
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+const MovieDetails = () => {
+  return (
+    <>
+      <h1>MovieDetails</h1>
+      <Outlet />
+    </>
+  );
+};
+
+export default MovieDetails;
